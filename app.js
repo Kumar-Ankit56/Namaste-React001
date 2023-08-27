b5abeb80a3af98d276b6e1f0deabf97f2883b60e
Ankit kumar <akg95469@gamil.com>
2023-08-27 11:32:31 +0530

    Nested Child and slibling element

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+//Creating two sibiling chiderean with two adject h1 element this is how we can do it.
+
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { id: "h1" }, "This is the h1 tag"),
     React.createElement("h2", { id: "h2" }, "This is the h2 tag"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "h1" }, "This is the h1 tag"),
+    React.createElement("h2", { id: "h2" }, "This is the h2 tag"),
+  ]),
+]);
 console.log(parent); //return Object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
